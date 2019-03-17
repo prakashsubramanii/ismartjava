1584fc00292db6f4b76542b9ab64b0b07be94319
Prakash <prakashooty16@gmail.com>
2019-03-17 13:22:43 +0530

    Committing Spend Category changes

--- ISmart/src/main/java/com/hackathon/ismart/controller/CustomerController.java
+++ ISmart/src/main/java/com/hackathon/ismart/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.hackathon.ismart.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +15,29 @@ import com.hackathon.ismart.model.Customer;
 import com.hackathon.ismart.service.CustomerService;
 
 @Controller
-@RequestMapping("/customers/api/")
+@RequestMapping("/api/customers")
 @CrossOrigin
 public class CustomerController {
 	
 	@Autowired
 	CustomerService customerService;
 	
-	@GetMapping
+	@GetMapping("/")
 	public ResponseEntity<List<Customer>> getAllCustomers(){
 		return new ResponseEntity<List<Customer>>(customerService.getCustomers(),HttpStatus.OK);
 	}
 
+	@GetMapping("/spendCategory/")
+	public ResponseEntity<List<String>> getSpendCategories(){
+		List<String> spendCategory = new ArrayList<>();
+		spendCategory.add("Medical");
+		spendCategory.add("Travel");
+		spendCategory.add("Loans");
+		spendCategory.add("Utility Bills");
+		spendCategory.add("Education");
+		spendCategory.add("Shopping");
+		spendCategory.add("Misc");
+		return new ResponseEntity<List<String>>(spendCategory,HttpStatus.OK);
+	}
 
 }
