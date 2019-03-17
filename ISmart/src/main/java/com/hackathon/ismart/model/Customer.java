cdf80d9f34c5eff608f0961d20ab8be0a25b4924
prakashsubramanii <prakashooty16.com>
2019-03-18 01:19:05 +0530

    Committing Entity classes

--- ISmart/src/main/java/com/hackathon/ismart/model/Customer.java
+++ ISmart/src/main/java/com/hackathon/ismart/model/Customer.java
@@ -1,7 +1,34 @@
 package com.hackathon.ismart.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
 	private Long customerID;
+	private double balance;
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Long getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(Long customerID) {
+		this.customerID = customerID;
+	}
 
 }
--- ISmart/src/main/java/com/hackathon/ismart/model/Transaction.java
+++ ISmart/src/main/java/com/hackathon/ismart/model/Transaction.java
@@ -1,5 +1,68 @@
 package com.hackathon.ismart.model;
 
-public class Transaction {
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Transaction {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="id")
+	private Long transactionId;
+	private String description;
+	private List<String> spendCategory;
+	private double amount;
+	private Date date;
+	private String paymentType;
+	private double balance;
+	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public Long getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(Long transactionId) {
+		this.transactionId = transactionId;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<String> getSpendCategory() {
+		return spendCategory;
+	}
+	public void setSpendCategory(List<String> spendCategory) {
+		this.spendCategory = spendCategory;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getPaymentType() {
+		return paymentType;
+	}
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+		
 }
